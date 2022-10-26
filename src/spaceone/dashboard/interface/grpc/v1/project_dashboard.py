eead55a03b8613fd74063d59d235629a32969033
Seolmin Kwon <seolmin@megazone.com>
2022-10-26 15:47:08 +0900

    feat: create interface of dashboard service (#1)
    Signed-off-by: Seolmin Kwon <seolmin@megazone.com>

---
+++ src/spaceone/dashboard/interface/grpc/v1/project_dashboard.py
@@ -0,0 +1,48 @@
+from spaceone.api.dashboard.v1 import project_dashboard_pb2, project_dashboard_pb2_grpc
+from spaceone.core.pygrpc import BaseAPI
+
+
+class ProjectDashboard(BaseAPI, project_dashboard_pb2_grpc.ProjectDashboardServicer):
+    pb2 = project_dashboard_pb2
+    pb2_grpc = project_dashboard_pb2_grpc
+
+    def create(self, request, context):
+        params, metadata = self.parse_request(request, context)
+
+        with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
+            return self.locator.get_info('ProjectDashboardInfo', project_dashboard_service.create(params))
+
+    def update(self, request, context):
+        params, metadata = self.parse_request(request, context)
+
+        with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
+            return self.locator.get_info('ProjectDashboardInfo', project_dashboard_service.update(params))
+
+    def delete(self, request, context):
+        params, metadata = self.parse_request(request, context)
+
+        with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
+            project_dashboard_service.delete(params)
+            return self.locator.get_info('EmptyInfo')
+
+    def get(self, request, context):
+        params, metadata = self.parse_request(request, context)
+
+        with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
+            return self.locator.get_info('ProjectDashboardInfo', project_dashboard_service.get(params))
+
+    def list(self, request, context):
+        params, metadata = self.parse_request(request, context)
+
+        with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
+            project_dashboard_vos, total_count = project_dashboard_service.list(params)
+            return self.locator.get_info('ProjectDashboardsInfo',
+                                         project_dashboard_vos,
+                                         total_count,
+                                         minimal=self.get_minimal(params))
+
+    def stat(self, request, context):
+        params, metadata = self.parse_request(request, context)
+
+        with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
+            return self.locator.get_info('StatisticsInfo', project_dashboard_service.stat(params))
