c797b1801b6b879fc4d8a54d61fca8929eee40df
Seolmin Kwon <seolmin@mz.co.kr>
2022-11-06 18:06:12 +0900

    feat: change tests according API docs in dashboard service (#1)
    Signed-off-by: Seolmin Kwon <seolmin@mz.co.kr>

--- test/factory/__init__.py
+++ test/factory/__init__.py
@@ -1,3 +1,3 @@
 from test.factory.domain_dashboard_factory import DomainDashboardFactory
 from test.factory.project_dashboard_factory import ProjectDashboardFactory
-from test.factory.widget_dashboard_factory import WidgetFactory
+from test.factory.custom_widget_dashboard_factory import CustomWidgetFactory
