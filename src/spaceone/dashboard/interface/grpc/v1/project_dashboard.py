9cd2fe813634149c9ad1a10cc8acccd1e24c14d0
Seolmin Kwon <seolmin@mz.co.kr>
2022-11-08 21:47:39 +0900

    feat: add interfaces of version history management in domain and project dashboard (#1)
    Signed-off-by: Seolmin Kwon <seolmin@megazone.com>

--- src/spaceone/dashboard/interface/grpc/v1/project_dashboard.py
+++ src/spaceone/dashboard/interface/grpc/v1/project_dashboard.py
@@ -31,6 +31,34 @@ class ProjectDashboard(BaseAPI, project_dashboard_pb2_grpc.ProjectDashboardServi
         with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
             return self.locator.get_info('ProjectDashboardInfo', project_dashboard_service.get(params))
 
+    def delete_version(self, request, context):
+        params, metadata = self.parse_request(request, context)
+
+        with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
+            return self.locator.get_info('ProjectDashboardInfo', project_dashboard_service.delete_version(params))
+
+    def revert_version(self, request, context):
+        params, metadata = self.parse_request(request, context)
+
+        with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
+            return self.locator.get_info('ProjectDashboardInfo', project_dashboard_service.revert_version(params))
+
+    def get_version(self, request, context):
+        params, metadata = self.parse_request(request, context)
+
+        with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
+            return self.locator.get_info('ProjectDashboardVersionInfo', project_dashboard_service.get_version(params))
+
+    def list_versions(self, request, context):
+        params, metadata = self.parse_request(request, context)
+
+        with self.locator.get_service('ProjectDashboardService', metadata) as project_dashboard_service:
+            project_dashboard_version_vos, total_count = project_dashboard_service.list_versions(params)
+            return self.locator.get_info('ProjectDashboardVersionsInfo',
+                                         project_dashboard_version_vos,
+                                         total_count,
+                                         minimal=self.get_minimal(params))
+
     def list(self, request, context):
         params, metadata = self.parse_request(request, context)
 
