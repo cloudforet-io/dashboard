d1f4b61330d4529389f457e82038ea7a762f3a42
seolmin <seolmin@megazone.com>
2025-04-23 20:40:41 +0900

    feat: refactor identity and cost analysis managers, remove unused methods and improve dashboard template structure
    Signed-off-by: seolmin <seolmin@megazone.com>

--- src/spaceone/dashboard/manager/cost_analysis_manager.py
+++ src/spaceone/dashboard/manager/cost_analysis_manager.py
@@ -13,67 +13,10 @@ class CostAnalysisManager(BaseManager):
         )
 
     def analyze_cost(self, params: dict) -> dict:
-        # params["query"] = self._change_filter_project_group_id(
-        #     params.get("query", {}), params.get("domain_id")
-        # )
         return self.cost_analysis_conn.dispatch("Cost.analyze", params)
 
     def analyze_unified_cost(self, params: dict) -> dict:
-        # params["query"] = self._change_filter_project_group_id(
-        #     params.get("query", {}), params.get("domain_id")
-        # )
         return self.cost_analysis_conn.dispatch("UnifiedCost.analyze", params)
 
     def list_data_sources(self, params: dict) -> dict:
         return self.cost_analysis_conn.dispatch("DataSource.list", params)
-
-    def _change_filter_project_group_id(self, query: dict, domain_id: str) -> dict:
-        change_filter = []
-
-        for condition in query.get("filter", []):
-            key = condition.get("k", condition.get("key"))
-            value = condition.get("v", condition.get("value"))
-            operator = condition.get("o", condition.get("operator"))
-
-            if key == "project_group_id":
-                # if self.identity_mgr is None:
-                #     self.identity_mgr: IdentityManager = self.locator.get_manager(
-                #         "IdentityManager"
-                #     )
-
-                project_groups_info = self.identity_mgr.list_project_groups(
-                    {
-                        "query": {
-                            "only": ["project_group_id"],
-                            "filter": [{"k": key, "v": value, "o": operator}],
-                        }
-                    },
-                    domain_id,
-                )
-
-                project_group_ids = [
-                    project_group_info["project_group_id"]
-                    for project_group_info in project_groups_info.get("results", [])
-                ]
-
-                project_ids = []
-
-                for project_group_id in project_group_ids:
-                    projects_info = self.identity_mgr.get_projects_in_project_group(
-                        project_group_id, domain_id
-                    )
-                    project_ids.extend(
-                        [
-                            project_info["project_id"]
-                            for project_info in projects_info.get("results", [])
-                        ]
-                    )
-
-                project_ids = list(set(project_ids))
-                change_filter.append({"k": "project_id", "v": project_ids, "o": "in"})
-
-            else:
-                change_filter.append(condition)
-
-        query["filter"] = change_filter
-        return query
--- src/spaceone/dashboard/manager/identity_manager.py
+++ src/spaceone/dashboard/manager/identity_manager.py
@@ -1,14 +1,11 @@
 from spaceone.core import config
 from spaceone.core.manager import BaseManager
-from spaceone.core.auth.jwt.jwt_util import JWTUtil
 from spaceone.core.connector.space_connector import SpaceConnector
 
 
 class IdentityManager(BaseManager):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        token = self.transaction.get_meta("token") or kwargs.get("token")
-        self.token_type = JWTUtil.get_value_from_token(token, "typ")
         self.identity_conn: SpaceConnector = self.locator.get_connector(
             "SpaceConnector", service="identity"
         )
@@ -36,27 +33,3 @@ class IdentityManager(BaseManager):
         else:
             query = {}
         return self.identity_conn.dispatch("ServiceAccount.list", query)
-
-    def list_project_groups(self, params: dict, domain_id: str) -> dict:
-        if self.token_type == "SYSTEM_TOKEN":
-            return self.identity_conn.dispatch(
-                "ProjectGroup.list", params, x_domain_id=domain_id
-            )
-        else:
-            return self.identity_conn.dispatch("ProjectGroup.list", params)
-
-    def get_projects_in_project_group(self, project_group_id: str, domain_id: str):
-        params = {
-            "query": {
-                "only": ["project_id"],
-            },
-            "project_group_id": project_group_id,
-            "include_children": True,
-        }
-
-        if self.token_type == "SYSTEM_TOKEN":
-            return self.identity_conn.dispatch(
-                "Project.list", params, x_domain_id=domain_id
-            )
-        else:
-            return self.identity_conn.dispatch("Project.list", params)
